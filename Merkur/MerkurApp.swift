3bc137f965243de3c08ebf0dab6958f8ae1a26e9
Alex <Lepiozdyx@gmail.com>
2024-12-17 10:06:10 -0300

    Initial Commit

---
+++ Merkur/MerkurApp.swift
@@ -0,0 +1,17 @@
+//
+//  MerkurApp.swift
+//  Merkur
+//
+//  Created by Alex on 17.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MerkurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
